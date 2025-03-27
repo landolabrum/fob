2600624700f0dea49949697512b06fc2331d5928
landolabrum <lando@deepturn.com>
2025-03-26 21:28:15 -0600

    lando 26.03.25-21:28:15

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+const Index = () => {
+    return (
+        <div>
+            <h1>My First Next.js App</h1>
+            <p>Welcome to my Next.js app!</p>
+        </div>
+    )
+}
+
+export default Index;
